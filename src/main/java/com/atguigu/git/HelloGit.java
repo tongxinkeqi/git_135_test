cda507c0d8c4690e90a4e1615a088d80ab3e701e
zhangsan <peter@atguigu.com>
2023-03-11 09:47:23 +0800

    第二次修改

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("第一次提交");
+        System.out.println("第二次更新");
     }
 }
